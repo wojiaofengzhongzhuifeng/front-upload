290bd5c5437466ebcb54aecd83a0542d7d3e78e5
饶家俊 <471412450@qq.com>
2021-04-16 15:15:32 +0800

    获取 zip 子节点

--- src/component/fileSystem/fileSystem.jsx
+++ src/component/fileSystem/fileSystem.jsx
@@ -1,60 +1,78 @@
 import React, {useEffect, useState} from 'react';
 import { Tree } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
 import {useFolderPathContext, getFolderSubContentList} from '../../utils/index';
 import { useHistory } from "react-router-dom";
 
 
+// It's just a simple demo. You can use tree map to optimize update perf.
+function updateTreeData(list, key, children) {
+  return list.map(node => {
+    if (node.key === key) {
+      return {
+        ...node,
+        children,
+      };
+    }
+    if (node.children) {
+      return {
+        ...node,
+        children: updateTreeData(node.children, key, children),
+      };
+    }
+    return node;
+  });
+}
+
 function FileSystemComponent(){
   const {folderPathList, setFolderPathList} = useFolderPathContext();
   let history = useHistory();
-  const [treeData, setTreeData] = useState([
-    {
-      title: "parent 1",
-      key: "0-0",
-      children: [
-
-      ]
-    }
-  ]);
+  const [treeData, setTreeData] = useState([]);
 
   useEffect(()=>{
-    async function getFolderSubContentListWrapper(){
-      if(folderPathList.length === 0){
-        history.push("/");
-        return
-      }
-      const subContentListResponse = await getFolderSubContentList(folderPathList);
-      const {data: subContentList} = subContentListResponse;
-
-      return subContentList
-    }
     getFolderSubContentListWrapper().then((subContentList)=>{
-      console.log(subContentList)
+      let needAppendTreeData = convertToTreeData(subContentList);
+      setTreeData(needAppendTreeData)
     })
+  }, [])
 
-  }, [folderPathList])
+  const convertToTreeData = (resData)=>{
+    let needAppendTreeData = resData && resData.map((subContent)=>{
+      let treeDataItem;
+      if(subContent.isDir && subContent.hasChildren){
+        treeDataItem = { title: subContent.name, key: subContent.name}
+      } else {
+        treeDataItem = { title: subContent.name, key: subContent.name, isLeaf: true  }
+      }
+      return treeDataItem
+    });
+    return needAppendTreeData
+  }
+  const getFolderSubContentListWrapper = async ()=>{
+    if(folderPathList.length === 0){history.push("/");return}
+    const subContentListResponse = await getFolderSubContentList(folderPathList);
+    const {data: subContentList} = subContentListResponse;
+    return subContentList
+  }
+  const onLoadData = ({ key, children }) => {
+    setFolderPathList([...folderPathList, key]);
+    return new Promise(resolve => {
+      if (children) {resolve();return;}
+      setFolderPathList(async current => {
+        const subContentListResponse = await getFolderSubContentList(current);
+        let needAppendTreeData = convertToTreeData(subContentListResponse.data);
 
-  const onSelect = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info);
-  };
-  const onExpand = (expandList, expandItem) => {
-    if(expandItem.expanded){
-      let folderPathName = expandItem.node.key;
-      setFolderPathList([...folderPathList, folderPathName])
-    }
-  };
+        setTreeData(origin =>
+          updateTreeData(origin, key, needAppendTreeData),
+        );
+        resolve();
+        return current
+      })
+    });
+  }
 
   return (
     <div style={{display:"flex"}}>
-      <Tree
-        showLine
-        switcherIcon={<DownOutlined />}
-        defaultExpandedKeys={['0-0-0']}
-        onSelect={onSelect}
-        treeData={treeData}
-        onExpand={onExpand}
-      />
+      <Tree loadData={onLoadData} treeData={treeData}/>
     </div>
   )
 }
